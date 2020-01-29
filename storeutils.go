2af45f863683c25bbfe265ae2fd039d6201cf9e0
Simon Tucker <simon@brotherlogic@gmail>
2020-01-28 18:24:41 -0800

    Working on getting mutations working.

--- storeutils.go
+++ storeutils.go
@@ -1,4 +1,22 @@
 package main
 
-func (s *Server) reallyDoNothing() {
+import (
+	"reflect"
+
+	"github.com/golang/protobuf/proto"
+
+	pb "github.com/brotherlogic/store/proto"
+)
+
+func (s *Server) applyMutation(base proto.Message, mutation *pb.Mutation) (proto.Message, error) {
+	val := reflect.ValueOf(base).Elem()
+
+	switch x := mutation.Value.(type) {
+	case *pb.Mutation_StringValue:
+		val.Field(int(mutation.GetField())).SetString(x.StringValue)
+	case *pb.Mutation_Int64Value:
+		val.Field(int(mutation.GetField())).SetInt(x.Int64Value)
+	}
+
+	return base, nil
 }
---
+++ storeutils_test.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"testing"
+	"time"
+
+	rcpb "github.com/brotherlogic/recordcollection/proto"
+	pb "github.com/brotherlogic/store/proto"
+)
+
+func TestApplyInt64Mutation(t *testing.T) {
+	s := InitTestServer()
+	s.doNothing()
+
+	tstart := time.Now().Unix()
+	adj := &rcpb.ReleaseMetadata{DateAdded: tstart}
+
+	ret, err := s.applyMutation(adj, &pb.Mutation{Field: int32(0), Value: &pb.Mutation_Int64Value{time.Now().Add(time.Minute).Unix()}})
+
+	if err != nil {
+		t.Errorf("Mutation was not applied")
+	}
+
+	adj2, ok := ret.(*rcpb.ReleaseMetadata)
+	if !ok {
+		t.Fatalf("Unable to channel %v", adj2)
+	}
+
+	if adj2.GetDateAdded() == tstart {
+		t.Errorf("Mutation error %v -> %v (%v)", ret, adj2, tstart)
+	}
+}
+
+func TestApplyStringMutation(t *testing.T) {
+	s := InitTestServer()
+	s.doNothing()
+
+	adj := &rcpb.ReleaseMetadata{FilePath: "first"}
+
+	ret, err := s.applyMutation(adj, &pb.Mutation{Field: int32(2), Value: &pb.Mutation_StringValue{"second"}})
+
+	if err != nil {
+		t.Errorf("Mutation was not applied")
+	}
+
+	adj2, ok := ret.(*rcpb.ReleaseMetadata)
+	if !ok {
+		t.Fatalf("Unable to channel %v", adj2)
+	}
+
+	if adj2.FilePath == "first" {
+		t.Errorf("Mutation error %v -> %v", ret, adj2)
+	}
+}
